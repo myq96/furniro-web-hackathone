bc312bafe5b74159528ae0720665494675e67d7a
Yasir Qureshi <myq1996@live.com>
2025-02-08 01:32:44 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import MainOne from "@/components/MainOne";
+import Products from "@/components/Products";
+import Slides from "@/components/Slides";
+import MainTwo from "@/components/MainTwo";
+
+export default function Home(){
+  return (
+    <>
+    <MainOne/>
+    <MainTwo/>
+    <Products/>
+    <Slides/>
+    </>
+  )
+}
